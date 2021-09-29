1946f91d58eeb092bcc049368fe9c7af87b758ac
Vladislav Kushch <kushch.vladislav@gmail.com>
2021-09-30 00:05:37 +0300

    SDK

---
+++ README.md
@@ -0,0 +1,320 @@
+Markdown output:
+# Accfarm SDK
+
+This is an official [Accfarm](https://accfarm.com/) Reseller SDK. Out of the box it covers all endpoints and has the functionality to fully interact with API. This SDK can be used for development as it is, or serve as reference for you own SDK.
+
+* [API Reference](https://documenter.getpostman.com/view/2711143/Tzz7QJNw#auth-info-17a939f6-551a-4217-83dd-bddcd305fa34)
+
+---
+
+### Table of contents
+
+
+* [Getting started](#getting-started)
+* [Methods](#methods)
+  * [Authentication](#authentication)
+    * [Authenticate](#authenticate)
+    * [Refresh](#refresh)
+    * [Invalidate](#invalidate)
+  * [Data](#data)
+    * [Offers](#offers)
+    * [Categories](#categories)
+    * [Orders](#orders)
+    * [Order](#order)
+    * [User](#user)
+  * [Buy](#buy)
+* [Callback](#callback)
+
+---
+
+### Getting started
+
+There are only 4 files in this SDK:
+
+* src/ResellerSDK/ResellerSDK.php - Contains all core code and logic
+* src/ResellerSDK/settings.json - Stores bearer token, user secret and endpoints
+* index.php - File used for demonstration (you can delete it)
+* callback_example.php - File used for demonstration (you can delete it)
+
+Let's start with our first and only `ResellerSDK` class.
+
+1. First of all, we need to create new object.
+
+```php
+require 'src\ResellerSDK\ResellerSDK.php';
+// and/or
+use ITAccfarm\ResellerSDK\ResellerSDK;
+
+$api = new ResellerSDK();
+```
+
+2. Then authenticate with email and password. And check if our credentials are correct.
+
+```php
+$success = $api->auth('email@email.com', 'pass');
+
+if (!$success) {
+    throw new Exception('Wrong credentials');
+}
+```
+
+3. Then we can make API requests! You can see the list of all methods in [Methods section](#methods).
+
+```php
+$categories = $api->categories();
+$offers = $api->offers(['product_id' => 4]);
+$user = $api->user();
+```
+
+---
+
+### Methods
+
+List of all methods:
+* `$api->auth(string $email, string $password): bool`
+* `$api->refresh(): bool`
+* `$api->invalidate(): bool`
+
+
+* `$api->uesr(): array`
+* `$api->categories(): array`
+* `$api->offers(array $filters): array`
+* `$api->orders(): array`
+* `$api->order(string $orderNumber): array`
+
+
+* `$api->buy(string $type, array $data)`
+
+
+* `$api->getUserSecret(string $type, array $data): string`
+
+
+### Authentication
+
+### Authenticate
+
+**Endpoint:** [https://accfarm.com/api/v1/user/login](https://accfarm.com/api/v1/user/login)  
+**Method:** `$api->auth(string $email, string $password)`  
+**Params:** `string $email, string $password`  
+**Returns:** bool (authentication successful or not)
+
+This method:
+
+1. Attempts authentication on 'user/login' endpoint.
+2. Stores
+
+_bearer token_ to settings.json and to object.
+
+After using this method successfully once _bearer token_ is stored, so you can call any api endpoints after that and don't need to call it every time you want to access some endpoint, until token expires.
+
+I would recommend using it once, like a setup method.
+
+### Refresh
+
+**Endpoint:** [https://accfarm.com/api/v1/user/refresh](https://accfarm.com/api/v1/user/refresh)  
+**Method:** `$api->refresh();`  
+**Returns: bool \(refresh successful or not\)** null
+
+This method:
+
+1. Refreshes token. 
+2. Replaces old token with a new one in settings.json and object.
+
+### Invalidate
+
+**Endpoint:** [https://accfarm.com/api/v1/user/invalidate](https://accfarm.com/api/v1/user/invalidate)  
+**Method:** `$api->refresh();`  
+**Returns: bool \(refresh successful or not\)** null
+
+This method:
+1. Invalidates and deletes token from object and settings.json.
+
+---
+
+### Data
+
+### User
+
+**Endpoint:** [https://accfarm.com/api/v1/user](https://accfarm.com/api/v1/user)  
+**Method:** `$api->user();`  
+**Returns:** array (current user)
+
+This method:
+
+1. Returns current user.
+
+### Offers
+
+**Endpoint:** [https://accfarm.com/api/v1/offers](https://accfarm.com/api/v1/offers)  
+**Method:** `$api->offers(array $data);`  
+**Params:** Optional: `[category_id => int, product_id => int, discount => bool]`  
+**Returns:** array (offers)
+
+This method:
+
+1. Return offers with filters.
+
+### Categories
+
+**Endpoint:** [https://accfarm.com/api/v1/categories](https://accfarm.com/api/v1/categories)  
+**Method:** `$api->categories();`  
+**Returns:** array (all categories)
+
+This method:
+
+1. Returns all categories.
+
+### Orders
+
+**Endpoint:** [https://accfarm.com/api/v1/orders](https://accfarm.com/api/v1/orders)  
+**Method:** `$api->orders();`  
+**Returns:** array (user orders)
+
+This method:
+
+1. Return all user orders.
+
+### Order
+
+**Endpoint:** [https://accfarm.com/api/v1/order](https://accfarm.com/api/v1/order)  
+**Method:** `$api->order(string $orderNumber);`  
+**Params:** `string $orderNumber`  
+**Returns:** array (user order)
+
+This method:
+
+1. Return order by it's 'order_number'.
+
+---
+
+### Buy
+
+**Endpoint:** [https://accfarm.com/api/v1/buy](https://accfarm.com/api/v1/buy)  
+**Method:** `$api->buy(string $type, array $data);`  
+**Returns:** array (user order)
+
+
+This method:
+
+1. Creates order and does everything a simple order creation on website would do.
+
+
+**Params for types:** 
+
+`$type` options: 
+1. `'offer'`
+2. `'review'`
+3. `'install'`
+
+`'offer'` type `$data` params:
+
+* quantity:          **required**|int
+* offer_id:          **required**|int
+* callback_url:      optional|string
+* sandbox:           optional|bool
+  
+`'review'` type `$data` params:
+
+* quantity:          **required**|int
+* offer_id:          **required**|int
+* url:               **required**|string
+* reviews_array:     optional|array
+* reviews:            optional|string
+* file:              optional|types:txt,doc,csv|max_size:6020
+* callback_url:      optional|string
+* sandbox:           optional|bool
+  
+`'install'` type `$data` params:
+
+* quantity:          **required**|int
+* offer_id:          **required**|int
+* app_link:          **required**|string
+* app_id:            **required**
+* days:              **required**|int
+* country:           **required**|array
+* reviews:            optional|string
+* file:              optional|types:txt,doc,csv|max_size:6020
+* callback_url:      optional|string
+* sandbox:           optional|bool
+
+If `'file'`, `'reviews'` or `'reviews_array'` is empty, then reviews will be autogenerated.
+It is recommended to write your on reviews.
+
+**Params in** `$data` **explained:**
+
+* `'file'`: send reviews in file. They will be reviewed manually.
+Example: `$data['file'] = realpath('') . '\' . 'test_data.txt;`
+* `'reviews'`: reviews should be divided by \n.
+Example: `$data['reviews'] = 'review1 \n review2 \n review3';`
+* `'reviews_array'`: each review in new index.
+Example: `$data['reviews_array'] = ['review1', 'review2', ...]`
+* `'url'`: url to place where to write reviews
+* `'app_link'`: url to place where to make installs
+* `'days'`: spread installations in this span of days
+* `'country'`: ISO country code (US, RU, UA, etc.)
+* `'sandbox'`: sandbox mode to make test orders.
+`$data['sandbox'] = 1;` for test orders.
+* `'callback_url'`: your url endpoint to send order data to.
+---
+
+### Callback
+
+If you provide callback_url in buy method, 
+Accfarm will send you order data on order updates.
+
+```php
+$response = [
+  'number' => 'order_number',
+  'status' => 'status_id',
+  'total' => 'price',
+  'secret_key' => 'secret_key'           
+  'download_link' - 'link' // if needed
+];
+```
+
+Call to you endpoint (your callback_url) will always have _**Signature**_ header.
+This will allow you to be sure call is coming from Accfarm.
+To check if it's valid you're going to need to: 
+1. Hash request data with your user secret with the following code:
+
+```php
+function signCallbackData(string $secret, array $data)
+{
+    ksort($data);
+
+    $string = '';
+
+    foreach($data as $value) {
+        if (in_array(gettype($value), ['array', 'object', 'NULL']) ){
+            continue;
+        }
+        if(is_bool($value) && $value){
+            $string .= 1;
+        } else {
+            $string .= $value;
+        }
+    }
+
+    return hash_hmac('sha512', strtolower($string), $secret);
+}
+```
+
+2. And then to check resulting hash against **_Signature_** header:
+
+```php
+$json = file_get_contents('php://input');
+$data = json_decode($json);
+$headers = getallheaders();
+
+$secret = 'my_secret';
+$testSignature = signCallbackData($secret, $data);
+$signature = $headers['Signature'];
+
+if (!hash_equals($signature, $testSignature)) {
+    // Error, wrong signature
+    die;
+}
+
+// Process data
+// ...
+```
---
+++ src/ResellerSDK/ResellerSDK.php
@@ -0,0 +1,732 @@
+<?php
+
+namespace ITAccfarm\ResellerSDK;
+
+/*
+|-------------------------------------------------------------------
+| Accfarm reseller Api SDK v1
+|-------------------------------------------------------------------
+|
+| This is the main (and only) class that helps you
+| work with Accfarm api. It allows you to basically use all
+| api endpoints and possibilities without writing your own SDK.
+| Or just to use it as an inspiration or point of reference,
+| while writing your own. We wish you best of luck!
+|
+ */
+
+class ResellerSDK
+{
+    /**
+     * Path to current directory
+     *
+     * @var string
+     */
+    private $dirPath;
+
+    /**
+     * Base Accfarm api url.
+     *
+     * @var string
+     */
+    private $baseUrl;
+
+    /**
+     * Authorization bearer token.
+     *
+     * @var string
+     */
+    private $bearerToken;
+
+    /**
+     * User secret to check callback signature.
+     *
+     * @var string
+     */
+    private $userSecret;
+
+    /**
+     * Settings from settings.json containing following parameters:
+     * ['bearerToken' => '', 'userSecret' => '', 'endpoints' => ['auth' => 'user/login', ...]]
+     *
+     * @var array
+     */
+    private $settings;
+
+    /**
+     * Api endpoints from settings
+     * ['auth' => 'user/login', ...]
+     *
+     * @var array
+     */
+    private $endpoints;
+
+    /**
+     * Api constructor.
+     *
+     * 1. Get path to class directory
+     * 2. Get settings.
+     * 3. Set base URL parameter.
+     * 4. Set token parameter.
+     * 5. Set endpoints parameter.
+     */
+    public function __construct()
+    {
+        $this->dirPath = dirname(__FILE__) . DIRECTORY_SEPARATOR;
+
+        $this->settings = $this->getFileSettings();
+
+        $this->baseUrl = 'https://accfarm.com/api/v1/';
+        $this->userSecret = $this->settings['secret'] ?? '';
+        $this->bearerToken = $this->settings['bearerToken'] ?? '';
+        $this->endpoints = $this->settings['endpoints'];
+    }
+
+    /**
+     * Authentication for user. Sets bearer token to settings.json file and to parameter.
+     *
+     * @param string $email
+     * @param string $password
+     * @return bool
+     */
+    public function auth(string $email, string $password): bool
+    {
+        $response = $this->authRequest([
+            'email' => $email,
+            'password' => $password,
+        ]);
+
+        if (empty($response['token']) || empty($response['user']) || empty($response['user']['secret'])) {
+            return false;
+        }
+
+        $this->bearerToken = $response['token'];
+        $this->userSecret = $response['user']['secret'];
+
+        $this
+            ->setSettingsToken()
+            ->setSettingsSecret()
+            ->setFileSettings();
+
+        return true;
+    }
+
+    /**
+     * Simply returns user secret for your further callback purposes.
+     *
+     * @return string
+     */
+    public function getUserSecret(): string
+    {
+        return $this->userSecret ?? '';
+    }
+
+    /**
+     * Returns available offers filtered by options.
+     *
+     * category_id:  int
+     * product_id:   int
+     * discount:     bool (false|0 - all or true|1 - only with discounts)
+     *
+     * Data example: ['category_id' => 1,'product_id' => 15, 'discount' => 1]
+     * @param array $data
+     * @return array
+     */
+    public function offers(array $data = []): array
+    {
+        $requestData = $this->validate($data, [
+            'category_id' => 'optional',
+            'product_id'  => 'optional',
+            'discount'    => 'optional',
+        ]);
+
+        return $this->getOffersRequest($requestData);
+    }
+
+    /**
+     * Returns all available categories to user.
+     *
+     * @return array
+     */
+    public function categories(): array
+    {
+        return $this->getCategoriesRequest();
+    }
+
+    /**
+     * Returns all user orders
+     *
+     * @return array
+     */
+    public function orders(): array
+    {
+        return $this->getOrdersRequest();
+    }
+
+    /**
+     * Returns all user orders
+     *
+     * @param string $orderNumber
+     * @return array
+     */
+    public function order(string $orderNumber): array
+    {
+        return $this->getOrderRequest(['order_number' => $orderNumber]);
+    }
+
+    /**
+     * Returns authenticated user data.
+     *
+     * @return array
+     */
+    public function user(): array
+    {
+        $user = $this->getUserRequest();
+
+        $this->userSecret = $user['secret'];
+
+        $this
+            ->setSettingsSecret()
+            ->setFileSettings();
+
+        return $user;
+    }
+
+    /**
+     * Refresh bearer token. Token has to be present.
+     *
+     */
+    public function refresh(): bool
+    {
+        if (empty($this->bearerToken)) {
+            return false;
+        }
+
+        $token = $this->refreshTokenRequest();
+
+        if (empty($token)) {
+            return false;
+        }
+
+        $this->bearerToken = $token;
+
+        $this
+            ->setSettingsToken()
+            ->setFileSettings();
+
+        return true;
+    }
+
+    /**
+     * Invalidates and unsets token.
+     *
+     * @return bool
+     */
+    public function invalidate(): bool
+    {
+        $success = $this->invalidateTokenRequest();
+
+        if (!$success) {
+            return false;
+        }
+
+        $this->bearerToken = '';
+
+        $this
+            ->setSettingsToken()
+            ->setFileSettings();
+
+        return true;
+    }
+
+    /**
+     * Types:
+     * 1. 'offer'
+     * 2. 'review'
+     * 3. 'install'
+     *
+     * 'offer' type $data params:
+     * - quantity:          required|int
+     * - offer_id:          required|int
+     * - callback_url:      optional|string
+     * - sandbox:           optional|bool
+     *
+     * 'review' type $data params:
+     * - quantity:          required|int
+     * - offer_id:          required|int
+     * - url:               required|string
+     * - reviews_array:     optional|array
+     * - reviews:            optional|string
+     * - file:              optional|types:txt,doc,csv|max_size:6020
+     * - callback_url:      optional|string
+     * - sandbox:           optional|bool
+     *
+     * 'install' type $data params:
+     * - quantity:          required|int
+     * - offer_id:          required|int
+     * - app_link:          required|string
+     * - app_id:            required
+     * - days:              required|int
+     * - country:           required|array
+     * - reviews:            optional|string
+     * - file:              optional|types:txt,doc,csv|max_size:6020
+     * - callback_url:      optional|string
+     * - sandbox:           optional|bool
+     *
+     * If 'file', 'review' or 'reviews_array' is empty, then reviews will be autogenerated.
+     * It is recommended to write your on reviews.
+     *
+     * Params in $data explained:
+     *
+     * 'file': send reviews in file. They will be reviewed manually.
+     * example: $data['file'] = realpath('') . '\' . 'test_data.txt;
+     *
+     * 'reviews': reviews should be divided by \n.
+     * example: $data['review'] = 'review1 \n review2 \n review3';
+     *
+     * 'reviews_array': each review in new index.
+     * example: $data['reviews_array'] = ['review1', 'review2', ...]
+     *
+     * 'url': url to place where to write reviews
+     *
+     * 'app_link': url to place where to make installs
+     *
+     * 'days': spread installations in this span of days
+     *
+     * 'country': ISO country code (US, RU, UA, etc.)
+     *
+     * 'sandbox': sandbox mode to make test orders.
+     * $data['sandbox'] = 1; for test orders.
+     *
+     * 'callback_url': your url endpoint to send order data to.
+     *
+     * @param string $type
+     * @param array $data
+     *
+     * @return ?array
+     */
+    public function buy(string $type, array $data): ?array
+    {
+        if ($type == 'offer') {
+            $requestData = $this->validate($data, [
+                'quantity'      => 'required',
+                'offer_id'      => 'required',
+                'callback_url'  => 'optional',
+                'sandbox'       => 'optional',
+            ]);
+        } elseif ($type == 'review') {
+            $requestData = $this->validate($data, [
+                'quantity'      => 'required',
+                'offer_id'      => 'required',
+                'url'           => 'required',
+                'reviews_array' => 'optional',
+                'reviews'       => 'optional',
+                'file'          => 'optional',
+                'callback_url'  => 'optional',
+                'sandbox'       => 'optional',
+            ]);
+        } elseif ($type == 'install') {
+            $requestData = $this->validate($data, [
+                'quantity'      => 'required',
+                'offer_id'      => 'required',
+                'app_link'      => 'required',
+                'app_id'        => 'required',
+                'days'          => 'required',
+                'country'       => 'required',
+                'reviews'       => 'optional',
+                'file'          => 'optional',
+                'callback_url'  => 'optional',
+                'sandbox'       => 'optional',
+            ]);
+        } else {
+            return null;
+        }
+
+        if (!empty($requestData['errors'])) {
+            return $requestData;
+        }
+
+        return $this->buyRequest($requestData);
+    }
+
+    /**
+     * Post request to 'https://accfarm.com/api/v1/user'
+     *
+     * Data example: ['email' => 'email@email.com', 'password' => 'pass']
+     *
+     * @param array $data
+     * @return array
+     */
+    private function authRequest(array $data): array
+    {
+        $urn = $this->endpoints['auth'];
+
+        $response = $this->call('POST', $urn, [
+            'email' => $data['email'],
+            'password' => $data['password'],
+        ], false);
+
+        if (empty($response)) {
+            return [];
+        }
+
+        return $response;
+    }
+
+    /**
+     * Get request to 'https://accfarm.com/api/v1/offers'
+     *
+     * category_id: int
+     * product_id:  int
+     * discount:    bool (0 or 1)
+     *
+     * Data example: ['category_id' => 1,'product_id' => 15, 'discount' => 1]
+     *
+     * @param array $data
+     * @return array
+     */
+    private function getOffersRequest(array $data = []): array
+    {
+        $urn = $this->endpoints['offers'];
+
+        $response = $this->call('GET', $urn, $data);
+
+        if (empty($response)) {
+            return [];
+        }
+
+        return $response;
+    }
+
+    /**
+     * Get request to 'https://accfarm.com/api/v1/categories'
+     *
+     * @return array
+     */
+    private function getCategoriesRequest(): array
+    {
+        $urn = $this->endpoints['categories'];
+
+        $response = $this->call('GET', $urn);
+
+        if (empty($response)) {
+            return [];
+        }
+
+        return $response;
+    }
+
+    /**
+     * Get request to 'https://accfarm.com/api/v1/orders'
+     *
+     * @return array
+     */
+    private function getOrdersRequest(): array
+    {
+        $urn = $this->endpoints['orders'];
+
+        $response = $this->call('GET', $urn);
+
+        if (empty($response)) {
+            return [];
+        }
+
+        return $response;
+    }
+
+    /**
+     * Get request to 'https://accfarm.com/api/v1/order'
+     *
+     * @param array $data
+     * @return array
+     */
+    private function getOrderRequest(array $data): array
+    {
+        $urn = $this->endpoints['order'];
+
+        $response = $this->call('GET', $urn, $data);
+
+        if (empty($response)) {
+            return [];
+        }
+
+        return $response;
+    }
+
+    /**
+     * Get request to 'https://accfarm.com/api/v1/user'
+     *
+     * @return array
+     */
+    private function getUserRequest(): array
+    {
+        $urn = $this->endpoints['user'];
+
+        $response = $this->call('GET', $urn);
+
+        if (empty($response)) {
+            return [];
+        }
+
+        return $response;
+    }
+
+    /**
+     * Post request to 'https://accfarm.com/api/v1/user/refresh'
+     *
+     * @return string
+     */
+    private function refreshTokenRequest(): string
+    {
+        $urn = $this->endpoints['refresh'];
+
+        $response = $this->call('POST', $urn, [
+            'token' => $this->bearerToken,
+        ], false);
+
+        if (!empty($response['error']) || empty($response['token'])) {
+            return '';
+        }
+
+        return $response['token'];
+    }
+
+    /**
+     * Post request to 'https://accfarm.com/api/v1/user/invalidate'
+     *
+     * @return bool
+     */
+    private function invalidateTokenRequest(): bool
+    {
+        $urn = $this->endpoints['invalidate'];
+
+        $response = $this->call('POST', $urn, [
+            'token' => $this->bearerToken,
+        ], false);
+
+        if (empty($response['error']) && !empty($response['msg']) && $response['msg'] == 'Token invalidated') {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param array $data
+     * @return array
+     */
+    private function buyRequest(array $data): array
+    {
+        $urn = $this->endpoints['buy'];
+
+        $response = $this->call('POST', $urn, $data);
+
+        if (empty($response)) {
+            return [];
+        }
+
+        return $response;
+    }
+
+    /**
+     * General curl api call method.
+     *
+     * Available methods: 'GET', 'POST'
+     * URN example: 'user/login'
+     * Data example: ['email' => 'blablabl@dev.null', 'password' => 'my_password']
+     *
+     * @param string $method
+     * @param string $urn
+     * @param array $data
+     * @param bool $useToken
+     *
+     * @return array|null
+     */
+    private function call(string $method, string $urn, array $data = [], bool $useToken = true): ?array
+    {
+        $url = $this->baseUrl . $urn;
+        $curlParams = [];
+        $headers = [];
+
+        // Check method
+        if ($method == 'GET') {
+            $url = !empty($data)
+                ? sprintf("%s?%s", $url, http_build_query($data))
+                : $url;
+
+            $headers[] = 'Content-Type: application/json';
+        } elseif ($method == 'POST') {
+            $curlParams += [
+                CURLOPT_POST => 1,
+            ];
+        }
+
+        // Check file sending required
+        if ($method == 'POST' && empty($data['file'])) {
+            $curlParams += [
+                CURLOPT_POSTFIELDS => json_encode($data),
+            ];
+
+            $headers[] = 'Content-Type: application/json';
+        } elseif ($method == 'POST' && !empty($data['file'])) {
+            $postFields = [
+                'file' => new CURLFile($data['file'], $this->getFileMimeType($data['file']), 'file'),
+            ];
+
+            $postFields += $data;
+
+            $curlParams += [
+                CURLOPT_POSTFIELDS => $postFields,
+            ];
+
+            $headers[] = 'Content-Type: multipart/form-data';
+        }
+
+        // Check if token present and required
+        if (!empty($this->bearerToken) && $useToken) {
+            $headers[] = 'Authorization: Bearer ' . $this->bearerToken;
+        }
+
+        $curlParams += [
+            CURLOPT_HEADER => 0,
+            CURLOPT_FRESH_CONNECT => 1,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_FORBID_REUSE => 1,
+            CURLOPT_TIMEOUT => 4,
+            CURLOPT_FOLLOWLOCATION => 0,
+            CURLOPT_HTTPHEADER => $headers,
+        ];
+
+        $curl = curl_init($url);
+        curl_setopt_array($curl, $curlParams);
+        $result = curl_exec($curl);
+        curl_close($curl);
+
+        if (empty($result)) {
+            return null;
+        }
+
+        return json_decode($result, true);
+    }
+
+    /**
+     * Gets data from settings.json
+     *
+     * @return array
+     */
+    private function getFileSettings(): array
+    {
+        if (!file_exists($this->dirPath . 'settings.json')) {
+            return [];
+        }
+
+        $settingsJson = file_get_contents($this->dirPath . 'settings.json');
+
+        return json_decode($settingsJson, true) ?? [];
+    }
+
+    /**
+     *  Clears settings.json file and sets new settings data from settings array.
+     *
+     * @return void
+     */
+    private function setFileSettings()
+    {
+        file_put_contents($this->dirPath . 'settings.json', '');
+        file_put_contents($this->dirPath . 'settings.json', json_encode($this->settings, JSON_PRETTY_PRINT));
+    }
+
+    /**
+     * Sets bearerToken to settings array.
+     *
+     * @return $this
+     */
+    private function setSettingsToken(): ResellerSDK
+    {
+        $this->settings['bearerToken'] = $this->bearerToken;
+
+        return $this;
+    }
+
+    /**
+     * Sets userSecret to settings array.
+     *
+     * @return $this
+     */
+    private function setSettingsSecret(): ResellerSDK
+    {
+        $this->settings['userSecret'] = $this->userSecret;
+
+        return $this;
+    }
+
+    /**
+     * @param string $pathToFile
+     * @return false|string
+     */
+    private function getFileMimeType(string $pathToFile)
+    {
+        return mime_content_type($pathToFile);
+    }
+
+    /**
+     * Available rules:
+     * 1. required or optional
+     *
+     * @param array $data
+     * @param array $rulesArray
+     * @return array
+     */
+    private function validate(array $data, array $rulesArray): array
+    {
+        $validated = [];
+        $errors = [];
+
+        foreach ($rulesArray as $field => $ruleString) {
+            $rules = explode('|', $ruleString);
+
+            foreach ($rules as $rule) {
+                $rule .= 'Rule';
+                $validatedRule = $this->$rule($field, $data);
+
+                if (!empty($validatedRule['error'])) {
+                    $errors += [$field => $validatedRule['error']];
+                } else {
+                    $validated += $validatedRule;
+                }
+            }
+        }
+
+        return !empty($errors)
+            ? ['errors' => $errors]
+            : $validated;
+    }
+
+    /**
+     * @param string $field
+     * @param array $data
+     * @return array
+     */
+    private function requiredRule(string $field, array $data): array
+    {
+        if (empty($data[$field])) {
+            return ['error' => $field . ' field is required!'];
+        }
+
+        return [$field => $data[$field]];
+    }
+
+    /**
+     * @param string $field
+     * @param array $data
+     * @return array
+     */
+    private function optionalRule(string $field, array $data): array
+    {
+        if (empty($data[$field])) {
+            return [];
+        }
+
+        return [$field => $data[$field]];
+    }
+}
